0a29f563506be2e035b1a2767aa1e7e4d4362fa3
Marco Scherzer <m.scherzer@hotmail.com>
2026-01-11 11:53:58 +0100

    continuing last commit, intermediate state

--- app/src/main/java/com/marcoscherzer/msimplespeechbackend/server/MISpeechRecognitionManager.java
+++ app/src/main/java/com/marcoscherzer/msimplespeechbackend/server/MISpeechRecognitionManager.java
@@ -5,6 +5,16 @@ import android.content.Intent;
 /**
  *  * @version 0.0.1 ,  unready intermediate state, @author Marco Scherzer, Author, Ideas, APIs, Nomenclatures & Architectures Marco Scherzer, Copyright Marco Scherzer, All rights reserved
  * Gemeinsames Interface für alle Speech-Recognition-Implementierungen.
+ * Bisher 2 Implementierungen
+ * MSpeechRecognitionManager
+ * MIntentBasedSpeechRecognitionManager
+ *
+ * Weitere Implemtierungen könnten theoretisch auch die neuen web speech-apis direkt nutzen,
+ * benutzung der on device speech recognition auf Android jedoch kostenlos und es ist nicht ausgeschlossen dass
+ * die speechRecognizer API bald intern die neuen web speech-apis oder lokal gemini nano nutzt.
+ * Direkte web API-Impl. für MISpeechRecognitionManager, dennoch nur sinnvoll wenn es Gründe gibt weswegen client device nicht direkt eine rest speech-api aufrufen sollte
+ * , da mit web API nutzung direkt auf client aktuell klarer was verwendet wird.
+ * (Eventuelle sinnhafte Anwendungfälle: nur bluetooth ohne netzwerk möglich, wenn client device unsicherer als server device bei direktem internet-access, etc.)
  * @version 1.1
  */
 public abstract class MISpeechRecognitionManager {
